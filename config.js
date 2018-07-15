eb8a5c9f9a9be7108377a10b5cd208acd965ac32
Nahuel Palumbo <nahuel.palumbo@gmail.com>
2018-07-15 17:19:51 +1200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 8080,
-  mongo: process.env.MONGODB_URI || 'mongodb://heroku_m6g9l2nt:is8trf9e5t6csdc25sb942err1@ds153890.mlab.com:53890/heroku_m6g9l2nt' || 'mongodb://localhost:27017/kiwi-bot',
+  mongo: process.env.MONGODB_URI || 'mongodb://localhost:27017/webapp',
   prerenderToken: process.env.PRERENDER_TOKEN || "u8Gm4855NKyjGr0T72OQ",
   credentials: {
     user: process.env.CRED_USER || 'hola',
